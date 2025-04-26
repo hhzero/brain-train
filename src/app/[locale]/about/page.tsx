8d15c2a3c36c67a814022420c2bb25812fa2ec27
zhzero <zhzero886@gmail.com>
2025-04-27 03:20:50 +0800

    更新关于页面的文本样式，将文本颜色调整为灰色，以提升可读性和视觉效果，同时保持国际化支持的一致性。

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -3,7 +3,7 @@ import { useTranslations } from 'next-intl'
 export default function About() {
   const t = useTranslations('')
   return (
-    <div className='px-32 py-24 text-center text-2xl'>
+    <div className='px-32 py-24 text-center text-2xl text-gray-500/70'>
       {t(
         'This_is_a_simple_about_page_just_to_show_how_you_can_use_the_internationalization_in_different_pages'
       )}
