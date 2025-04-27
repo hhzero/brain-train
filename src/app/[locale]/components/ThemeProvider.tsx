d689a354dd2c049d5980377b0168ddd64b18c094
zhzero <zhzero886@gmail.com>
2025-04-27 23:59:22 +0800

    更新主题提供者组件，移除默认主题中的“系统”选项，并将其设置为“默认主题”。此更改旨在简化主题选择，提升用户体验。

--- src/app/[locale]/components/ThemeProvider.tsx
+++ src/app/[locale]/components/ThemeProvider.tsx
@@ -5,7 +5,8 @@ import { type ThemeProviderProps } from 'next-themes/dist/types'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider 
-    themes={['system', 'light', 'dark', 'sunset', 'instagram', 'facebook', 'discord', 'netflix', 'reddit', 'twilight']} 
+    themes={['light', 'dark', 'sunset', 'instagram', 'facebook', 'discord', 'netflix', 'reddit', 'twilight']} 
+    defaultTheme="system"
     {...props}
   >
     {children}
