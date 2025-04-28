e9b286024083412f3c83836fd184ad5db0e9c67a
zhzero <zhzero886@gmail.com>
2025-04-29 00:18:54 +0800

    更新多语言支持，优化关于页面的文本内容，添加持续更新和新功能的说明，以提升用户体验和国际化支持。同时，调整训练页面的卡片样式，确保视觉一致性和响应式设计。

--- src/app/[locale]/reaction/page.tsx
+++ src/app/[locale]/reaction/page.tsx
@@ -31,7 +31,7 @@ export default function ReactionSpeed({
       <h1 className='mb-8 text-3xl font-bold text-gray-300'>{reactionData.name}</h1>
       <div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'>
         {reactionData.list.map((item: any) => (
-          <div key={item.id} className='rounded-lg bg-card p-6 shadow-md bg-black/30'>
+          <div key={item.id} className='rounded-lg bg-card p-6 shadow-md bg-black/40'>
             <h2 className='mb-4 text-xl font-semibold text-gray-300'>{item.name}</h2>
             <p className='mb-4 text-cyan-300'>{item.description}</p>
             <div className='mt-4 flex justify-end'> 
