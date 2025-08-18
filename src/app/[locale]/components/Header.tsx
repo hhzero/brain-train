95c7c2482141db6419a1e69600e96b9a86d6d4af
zhzero <zhzero886@gmail.com>
2025-08-18 21:26:35 +0800

    docs(Header): 将品牌名称从"Zero Brain"更改为"Brain Train"

--- src/app/[locale]/components/Header.tsx
+++ src/app/[locale]/components/Header.tsx
@@ -32,7 +32,7 @@ export const Header: FC<Props> = ({ locale }) => {
           <div className='mb-2 h-14 w-14'>
             <LogoIcon />
           </div>
-          <strong className='mx-2 select-none text-white'>Zero Brain</strong>
+          <strong className='mx-2 select-none text-white'>Brain Train</strong>
         </div>
       </Link>
       <div className='flex flex-row items-center gap-3'>
