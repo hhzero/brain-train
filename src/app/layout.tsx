366d335a3c224919ae3a3c5191e4daca226c732f
zhzero <zhzero886@gmail.com>
2025-08-21 18:56:18 +0800

    feat: 添加Vercel Analytics跟踪

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './[locale]/globals.css'
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'Brain Train',
